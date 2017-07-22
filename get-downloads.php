3934a3f6ae140458ee30feb611f2a10e34cb7669
nolaandy <andyw504@gmail.com>
2017-07-22 13:01:53 -0500

    change json url

--- get-downloads.php
+++ get-downloads.php
@@ -1,5 +1,5 @@
 <?php
-  $file = "downloads.json";
+  $file = "https://p3lican.github.io/boatcolor/downloads.json";
   $json = json_decode(file_get_contents($file), true);
   header('Content-Type: application/json');
   echo json_encode($json);
