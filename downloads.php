475c7fcde4f4ef34878968552d1ebe51b9e0a845
nolaandy <andyw504@gmail.com>
2017-07-22 12:54:47 -0500

    Initial load

---
+++ downloads.php
@@ -0,0 +1,10 @@
+<?php
+  $file = "downloads.json";
+  $json = json_decode(file_get_contents($file), true);
+
+  $json['redCount'] = $json['redCount'] + 1;
+  $json['whiteCount'] = $json['whiteCount'] + 1;
+  $json['blueCount'] = $json['blueCount'] + 1;
+  file_put_contents($file, json_encode($json));
+  echo 'success';
+?>
